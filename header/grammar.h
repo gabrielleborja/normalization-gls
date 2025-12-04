45f8ab8f3deb6791bdc111516f330e0b879458fa
Lucas Dantas <lucasvdmedeiros@proton.me>
2025-12-04 07:01:36 -0300

    chore: update gitignore

--- header/grammar.h
+++ header/grammar.h
@@ -11,19 +11,19 @@
 using namespace std;
 
 class Grammar{
-private:
-    set<string> terminals;
-    string startSymbol;
-    map<string, set<vector<string>>> productions;
-public:
-    Grammar();
-    Grammar(string startSymbol, set<string> terminals);
+    private:
+        set<string> terminals;
+        string startSymbol;
+        map<string, set<vector<string>>> productions;
+    public:
+        Grammar();
+        Grammar(string startSymbol, set<string> terminals);
 
-    void addProduction(string A, vector<string> rhs);
-    void removeProduction(string A, vector<string> rhs);
-    set<vector<string>> getProductions(string A);
-    void print(ostream &out);
-    Grammar clone() const; // Modified signature to be a const member function returning a copy
+        void addProduction(string A, vector<string> rhs);
+        void removeProduction(string A, vector<string> rhs);
+        set<vector<string>> getProductions(string A);
+        void print(ostream &out);
+        Grammar clone() const;
 };
 
 #endif
